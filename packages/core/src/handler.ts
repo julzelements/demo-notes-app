978ea5be8a0da2a5bc0731e9bf2d70cc6a3877b7
Julian Scharf <julzelements@gmail.com>
2023-05-06 19:56:19 +1000

    enabling cors

--- packages/core/src/handler.ts
+++ packages/core/src/handler.ts
@@ -14,8 +14,12 @@ export default function handler(lambda: any) {
         }
 
         return {
-            statusCode: 200,
-            body: JSON.stringify(body)
-        }
+            statusCode,
+            body: JSON.stringify(body),
+            headers: {
+              "Access-Control-Allow-Origin": "*",
+              "Access-Control-Allow-Credentials": true,
+            },
+          };
     }
 }
