c6b0b50e36c46c119f807329620f5163619f54b4
Julian Scharf <julzelements@gmail.com>
2023-05-07 17:44:28 +1000

    frontend

--- sst.config.ts
+++ sst.config.ts
@@ -2,6 +2,7 @@ import { SSTConfig } from "sst";
 import { StorageStack } from "./stacks/StorageStack"
 import { ApiStack } from "./stacks/ApiStack";
 import { AuthStack } from "./stacks/AuthStack";
+import { FrontendStack } from "./stacks/FrontendStack";
 
 export default {
   config(_input) {
@@ -18,5 +19,6 @@ export default {
     .stack(StorageStack)
     .stack(ApiStack)
     .stack(AuthStack)
+    .stack(FrontendStack)
   }
 } satisfies SSTConfig;
